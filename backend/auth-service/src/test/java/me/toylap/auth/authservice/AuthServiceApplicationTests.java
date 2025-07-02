8c763cb2eedd907a24938ef06dc6c5766d6b5bf2
최민수 <chlalstn0123@gmail.com>
2025-07-02 21:31:04 +0900

    초기설정작업

---
+++ backend/auth-service/src/test/java/me/toylap/auth/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.toylap.auth.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
